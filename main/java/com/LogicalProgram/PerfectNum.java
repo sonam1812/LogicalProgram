a2417b123c4f80d7efaf8dd66f0b2c76b1c1666e
sonam1812 <sonampal832@gmail.com>
2021-09-01 14:05:17 +0530

    Added: Perfect Number Program

---
+++ main/java/com/LogicalProgram/PerfectNum.java
@@ -0,0 +1,25 @@
+package com.LogicalProgram;
+import java.util.Scanner;
+public class PerfectNum {
+	public static void main(String[] args) {
+
+	  System.out.print("Enter the number: ");
+	  Scanner numberScan = new Scanner(System.in);
+      int number = numberScan.nextInt();
+      int sum = 0;
+      int i = 1;
+	  while (i <= number / 2) {
+	  if (number % i == 0) {
+
+	    sum = sum + i;
+	   }
+	      i++;
+  }
+	  if (sum == number) {
+	  System.out.println(number + " is a perfect number.");
+	        } else {
+	       System.out.println(number + " is not a perfect number.");
+	    }
+	}
+}
+
